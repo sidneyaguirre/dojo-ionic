dbf5f25af6b2c10882cc10cb9c919c7e82b47e3d
juancardona11 <juan.cardona11@udea.edu.co>
2019-07-13 09:37:37 -0500

    app

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera } from "@ionic-native/camera/ngx";
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,34 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  photo: any;
+  constructor(private camera: Camera) {}
+  capturePhoto(){
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.CAMERA,
+    }).then(dataImage => {
+      this.photo = 'data:image/jpeg;base64,' + dataImage;
+    });
+  }
+
+  searchPhoto(){
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+    }).then(dataImage => {
+      this.photo = 'data:image/jpeg;base64,' + dataImage;
+    });
+  }
 
 }
